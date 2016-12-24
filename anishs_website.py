0200635ee90f96233f908d0bee8587c050a0c0fa
ravin thambapillai <rthambapillai@palantir.com>
2016-12-24 19:38:45 +0530

    fix indentation bug 3

--- anishs_website.py
+++ anishs_website.py
@@ -13,7 +13,7 @@ def anish_related_things():
 def other_thing():
 	return "Hi, I am more shenanigans"
 
- if __name__ == '__main__':
+if __name__ == '__main__':
  	app.debug = True
  	port = int(os.environ.get("PORT", 5000))
  	app.run(host='0.0.0.0', port=port)
