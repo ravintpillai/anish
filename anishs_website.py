b67e05286674a263467910257228d1e677aebc73
Anish Mariathasan <anishmariathasan@gmail.com>
2017-10-23 20:30:28 +0100

    Update anishs_website.py

--- anishs_website.py
+++ anishs_website.py
@@ -1,3 +1,16 @@
+<!doctype html>
+<html>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-107714833-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-107714833-1');
+</script>
+
+
 from flask import Flask, render_template
 import os
 
