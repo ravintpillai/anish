11854bf64db5d16b04128eb2e036539313909bf8
Anish Mariathasan <anishmariathasan@gmail.com>
2017-10-24 18:01:10 +0100

    Update anishs_website.py

--- anishs_website.py
+++ anishs_website.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	return render_template('index.html')
-	return render_template('contact.html')
+	
 
 
 @app.route('/anish_related_things')
@@ -16,10 +16,15 @@ def anish_related_things():
 @app.route('/about')
 def other_thing():
 	return "This fabulous website was made by a person, who got the domain as a gift from an epic guy..."
+	
 
 @app.route('/contact')
 def contact_thing():
 	return "I am impossible to contact. Sadly. But, if you know how to navigate the internet, you can find me..."
+@app.route('/play')
+def fun_thing():
+	return "Pplay this game about this website..."
+	return render_template('contact.html')
 
 if __name__ == '__main__':
  	app.debug = True
